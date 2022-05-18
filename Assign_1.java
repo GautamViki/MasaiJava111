9ff0151e07e97083659a87a45c178a84a6548bed
GautamViki <vikacse@gmail.com>
2022-05-18 22:13:35 +0530

    fdj

---
+++ Assign_1.java
@@ -0,0 +1,13 @@
+package com.masaijava111;
+
+public class Assign_1 {
+	public static void main(String[] args) {
+		System.out.println("Nmae : Vikas Kumar Gautam");
+		System.out.println("Father’s Nmae : Mr. Ram Adhar");
+		System.out.println("Mother ‘s Nmae : Mrs. Rama Devi");
+		System.out.println("Age : 26 years");
+		System.out.println("Gender  : Male");
+		System.out.println("Address   : Uttar Pradesh");
+		System.out.println("Mobile No.  : 8957411197");
+	}
+}
---
+++ Demo.java
@@ -0,0 +1,11 @@
+package com.masaijava111;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("welcome");
+	}
+
+}
